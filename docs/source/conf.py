fc7398a6a47305b7c42c3138cd85354ea0e20c73
Dror Tirosh <dror@opengsn.org>
2022-05-24 03:10:15 +0300

    project info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Account Abstraction'
+copyright = '2022, Infinitism'
+author = 'Dror Tirosh'
 
 release = '0.1'
 version = '0.1.0'
